0a9e80966d23427d1af1c096f9d4f17f321805f8
simon <xiaoming.liao@medbanks.cn>
2021-08-20 15:53:24 +0800

    bug fix

--- src/components/layout/footer.vue
+++ src/components/layout/footer.vue
@@ -2,8 +2,8 @@
     <div class="footer-wrape">
         <div class="content">
             免责声明：本站部分资源来源于网络，版权归原作者所有，若有侵犯您的权利，请来信告知，
-            我们将立即予以删除。邮箱：abcde@qq.com 万分感谢！<br>
-            ©xxx网 京ICP备20200xx号-1
+            我们将立即予以删除。邮箱：abcde@？？.com 万分感谢！<br>
+            <!--©xxx网 京ICP备20200xx号-1-->
         </div>
     </div>
 </template>
